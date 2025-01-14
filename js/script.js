146bac67e30238692b58db40c2a7fdc74da32d63
Yifan-Zeng0059 <zeng0059@algonquinlive.com>
2025-01-14 14:58:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+//Task 1
+let a = 3;
+let b = 5;
+let c;
 
+let output1 = `
+let a = 3;
+let b = 5;
+let c;
+
+a + b = ${a + b};
+a - b = ${a - b};
+a * b = ${a * b};
+a / b = ${a / b};
+a % b = ${a % b};
+a += b = ${a += b};
+a -= b = ${a -= b};
+a *= b = ${a *= b};
+a /= b = ${a /= b};
+a %= b = ${a %= b};
+a == b = ${a == b};
+a != b = ${a != b};
+a > b = ${a > b};
+a < b = ${a < b};
+!a && !c = ${!a && !c};
+!a || !c = ${!a || !c};
+`
+
+console.log(output1);
+
+//Task 2
+let first_name = "Yifan";
+let last_name = "Zeng";
+let email = "zeng0059@algonquinlive.com";
+//let output;
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
